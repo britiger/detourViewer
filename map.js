507ae26e1acc0c8220d4fbf41e48e70fb5249f91
Christopher Lorenz <britiger@gmx.net>
2014-07-25 18:27:28 +0200

    Fix #2 : Correct some errors, tested Browsers: IE9, FF24, FF25, FF31

--- map.js
+++ map.js
@@ -14,28 +14,23 @@ function showRel(id) {
 	$.ajax({
 		dataType: "json",
 		url: url,
-		success: displayData
+		success: displayData,
+		error: errorAjax
 	});
 }
 
 function displayData(data) {
-	// Remove old Layer if exists
-	if(opl)	map.removeLayer(opl);
-	// Adding Layer
-	opl = L.geoJson(data, {style: styleRelation, pointToLayer: startEndPoint });
-	opl.addTo(map);
-	map.fitBounds(opl.getBounds());
-
-	// Show Data in Info-Box
+	// Check Response for data
 	if(!data.features[0]) {
-		alert("Error loading Data.");
+		alert("Error receiving data from Overpass API.");
 		return; // No Data
 	}
 
+	// Show Data in Info-Box
 	var keyVal = data.features[0].properties;
 	var id = keyVal["relation"];
 
-	var html = '<h3>OSM Realtion <a href="https://www.openstreetmap.org/relation/'+id+'" target="_blank">'+id+'</a></h3><ul>';
+	var html = '<h3>OSM Relation <a href="https://www.openstreetmap.org/relation/'+id+'" target="_blank">'+id+'</a></h3><ul>';
 
 	for (var k in keyVal){
 		if(k == 'id' || k == 'role' || k == 'relation' || k == 'member')
@@ -45,9 +40,26 @@ function displayData(data) {
 
 	html += '</ul>';
 	$('#info').html(html);	
+
+	// Remove old Layer if exists
+	if(opl)	map.removeLayer(opl);
+
+	// Adding Layer
+	opl = new L.geoJson(data, {style: styleRelation, pointToLayer: startEndPoint });
+	opl.addTo(map);
+	map.fitBounds(opl.getBounds());
+
+
+
 	
 }
 
+// Error handling
+function errorAjax(xhr, status, error) {
+	alert("Error receiving data from Overpass API.");
+}
+
+// format nodes in Realtion
 function startEndPoint(feature, latlng) {
 	var color = 'blue';
 	var ch = 'unk';
@@ -60,17 +72,18 @@ function startEndPoint(feature, latlng) {
 		ch = 'e';
 	}
 
-	var mark = L.AwesomeMarkers.icon({
+	var mark = new L.AwesomeMarkers.icon({
 		markerColor: color, prefix: 'ch', icon: ch
 	});
 	return new L.Marker(latlng, { title: feature.properties.role, icon: mark });
 }
 
+// Style detour roads by type 
 function styleRelation (feature) {
 	if(feature.properties.detour) {
-		if(feature.properties.detour.startsWith("A"))
+		if(feature.properties.detour.substr(0,1) == "A")
 			return {color: "blue",  opacity: 1 };
-		if(feature.properties.detour.startsWith("B"))
+		if(feature.properties.detour.substr(0,1) == "B")
 			return {color: "orange",  opacity: 1 };
 	}
         return {color: "red",  opacity: 1 };
