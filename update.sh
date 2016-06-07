acd8f98fc3cfa7c496ad295e6544177e3e62bb40
Christopher Lorenz <britiger@gmx.net>
2016-06-07 21:29:59 +0200

    Adding timeout for update

--- update.sh
+++ update.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+cd /var/customers/webs/clorenz/detourViewer
 # Overpass QL for all detour relations:
 # relation[route=detour];
 # out;
 wget http://overpass-api.de/api/interpreter?data=relation%5Broute%3Ddetour%5D%3B%0Aout%3B -O detour-all.xml
 
 # Overpass QL Germany only:
+# [timeout:600];
 # relation["route"="detour"](area:3600051477);
 # out;
-wget http://overpass-api.de/api/interpreter?data=relation%5B%22route%22%3D%22detour%22%5D\(area%3A3600051477\)%3B%0Aout%3B -O detour.xml
+wget http://overpass-api.de/api/interpreter?data=%5Btimeout%3A600%5D%3B%0Arelation%5B%22route%22%3D%22detour%22%5D\(area%3A3600051477\)%3B%0Aout%3B -O detour.xml
